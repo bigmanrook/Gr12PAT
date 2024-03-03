7c21625cde7642b985cb733ba4475a271c3fe18f
bigmanrook <94313617+bigmanrook@users.noreply.github.com>
2024-03-03 16:15:38 +0200

    Attempting to stage images for commit

---
+++ Database_dm.dfm
@@ -0,0 +1,9 @@
+object DataModule1: TDataModule1
+  OnCreate = DataModuleSetup
+  Height = 480
+  Width = 640
+  object ADOConnection1: TADOConnection
+    Left = 280
+    Top = 264
+  end
+end
---
+++ Property_cls.pas
@@ -0,0 +1,29 @@
+unit Property_cls;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls;
+
+
+type
+  TProperty = class(TObject)
+
+  private
+    { Private declarations }
+    fPropertyNo : Integer;
+    fPropertyArea : Real;
+    fPropertyLocation : String;
+
+
+
+  public
+    { Public declarations }
+
+  end;
+
+
+implementation
+
+end.
