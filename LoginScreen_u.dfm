14a0fda321ec53f151ac31f8a17eb7d77e431d99
Jude Wells <Jude Wells>
2024-03-28 06:18:13 +0200

    added object creation code

--- LoginScreen_u.dfm
+++ LoginScreen_u.dfm
@@ -2,8 +2,8 @@ object frmLoginScreen: TfrmLoginScreen
   Left = 0
   Top = 0
   Caption = 'Login Screen'
-  ClientHeight = 429
-  ClientWidth = 606
+  ClientHeight = 420
+  ClientWidth = 600
   Color = clBtnFace
   Font.Charset = DEFAULT_CHARSET
   Font.Color = clWindowText
--- LoginScreen_u.pas
+++ LoginScreen_u.pas
@@ -4,7 +4,7 @@ interface
 
 uses
   Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
-  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls, Database_dm, SignUp_u;
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls, Database_dm, SignUp_u, User_cls;
 
 type
   TfrmLoginScreen = class(TForm)
@@ -23,6 +23,7 @@ type
     { Public declarations }
 
     sPassword, sUserAcc : String;
+    User : TUser;
   end;
 
 var
@@ -38,6 +39,7 @@ uses MainMenu_u ;
 procedure TfrmLoginScreen.btnLoginClick(Sender: TObject);
 var
 bLogin : boolean;
+sMissing : String;
 begin
   bLogin := False;
  with Database_dm.DataModule1 do
@@ -58,11 +60,27 @@ begin
 
          if ((edtAccount.Text = TblAcc['Account']) OR (edtAccount.Text = TblAcc['User Alias'])) AND (edtPassword.Text = tblAcc['Password']) then
           begin
+            if edtAccount.Text = TblAcc['Account'] then
+              begin
 
-            BLogin := true;
-            frmLoginScreen.Visible := False;
-            frmMainMenu.Visible := True;
-            //Make object attributes equal to their corresponding variables, to create a User object, or even assign everything to an string array and then convert to respective values...
+                sMissing := tblAcc['User Alias'];
+                User := TUser.Create(sUserAcc, sMissing, sPassword, tblAcc['Property No'], tblAcc['Net Value'], tblAcc['Renting']);
+                BLogin := true;
+                frmLoginScreen.Visible := False;
+                frmMainMenu.Visible := True;
+                //Make object attributes equal to their corresponding variables, to create a User object, or even assign everything to an string array and then convert to respective values...
+               end;
+            if edtAccount.Text = TblAcc['User Alias'] then
+              begin
+
+                sMissing := tblAcc['Account'];
+                User := TUser.Create(sMissing, sUserAcc, sPassword, tblAcc['Property No'], tblAcc['Net Value'], tblAcc['Renting']);
+                BLogin := true;
+                frmLoginScreen.Visible := False;
+                frmMainMenu.Visible := True;
+
+
+              end;
 
           end
           else
