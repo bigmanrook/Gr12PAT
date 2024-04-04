4ef011226618d73de297d8e26921171e043054e8
bigmanrook <94313617+bigmanrook@users.noreply.github.com>
2024-04-04 20:35:07 +0200

    Added new unit file PropertyView_u

---
+++ PropertyView_u.pas
@@ -0,0 +1,26 @@
+unit PropertyView_u;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Data.DB, Data.Win.ADODB, Database_dm,
+  Vcl.Grids;
+
+type
+  TForm1 = class(TForm)
+    StringGrid1: TStringGrid;
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.dfm}
+
+end.
